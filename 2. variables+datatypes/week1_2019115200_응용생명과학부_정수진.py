d0a00ccaceb1a8da0766ec520beb5ab7a0169565
Int-TRUE <dmg06017@naver.com>
2021-07-20 23:02:27 +0900

    first commit

---
+++ 2. variables+datatypes/week1_2019115200_응용생명과학부_정수진.py
@@ -0,0 +1,10 @@
+# 개인정보 및 목표
+name = "정수진"
+major = "응용생명과학부"
+grade = "3학년"
+goal = "파이썬부터 데이터 실습까지 끝까지 완주해서 실력 up 하겠습니다!"
+a = f'{goal}' # string interpolation
+
+print(f'경북대학교 {major} {grade} {name}은 이번 SW융합교육을 통해')
+# print("경북대학교 {1} {2} {0}은 이번 SW융합교육을 통해".format(name, major, grade))
+print(a)
